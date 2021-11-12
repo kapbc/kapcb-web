bc95a41a05b45b78bd1f85e75f90fc1acfa2656a
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-11-12 15:40:37 +0800

    response

--- src/main/java/com/kapcb/framework/web/util/ResponseUtil.java
+++ src/main/java/com/kapcb/framework/web/util/ResponseUtil.java
@@ -33,10 +33,12 @@ public class ResponseUtil {
     @NonNull
     public static void setHeader(@NonNull HttpServletResponse httpServletResponse, @Nullable String type) {
         // default png
-        httpServletResponse.setContentType(MediaType.IMAGE_PNG_VALUE);
         if (StringUtils.equalsAnyIgnoreCase(type, StringPool.IMAGE_SUFFIX_GIF.value())) {
             httpServletResponse.setContentType(MediaType.IMAGE_GIF_VALUE);
+        } else {
+            httpServletResponse.setContentType(MediaType.IMAGE_PNG_VALUE);
         }
+
         httpServletResponse.setHeader(HttpHeaders.PRAGMA, StringPool.RESPONSE_HEAD_NO_CACHE.value());
         httpServletResponse.setHeader(HttpHeaders.CACHE_CONTROL, StringPool.RESPONSE_HEAD_NO_CACHE.value());
         httpServletResponse.setDateHeader(HttpHeaders.EXPIRES, LongPool.ZERO.value());
