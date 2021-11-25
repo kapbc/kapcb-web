b2842251cd95d138fa3f99fab51100b5764f04b3
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-11-25 22:04:40 +0800

    log

---
+++ src/main/java/com/kapcb/framework/web/model/logging/ILog.java
@@ -0,0 +1,15 @@
+package com.kapcb.framework.web.model.logging;
+
+/**
+ * <a>Title: ILog </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: ILog <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/11/25 22:04
+ */
+public interface ILog {
+
+
+}
